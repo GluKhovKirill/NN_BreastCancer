fe36f0c59bdafc27cbac73059c49f9f9ad42ce06
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2021-12-03 23:58:45 +0300

    Added model import

--- server.py
+++ server.py
@@ -4,9 +4,12 @@ import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    with open('model.pckl', 'rb') as file:
+        MODEL = pickle.loads(file.read())
     app.run(host='0.0.0.0', port='4862')
