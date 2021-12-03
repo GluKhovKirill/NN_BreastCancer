1b98baf9802a3cf7424b35f6661d4defe7c82c2d
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2021-12-03 23:52:31 +0300

    Added test server part

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import pickle
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='4862')
